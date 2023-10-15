a137c463f92645ebc7876bc65b7c788d3861f860
CrispyBun <74562111+CrispyBun@users.noreply.github.com>
2023-10-16 00:24:13 +0200

    Defining classes

--- lass.lua
+++ lass.lua
@@ -18,9 +18,116 @@ local function assertType(value, desiredType, errorMessage, errorLevel)
     end
 end
 
--- The data that makes Lass churn ------------------------------------------------------------------
+-- Definitions -------------------------------------------------------------------------------------
 
+---@class LassVariableDefinition
+---@field accessLevel "private"|"public"
+---@field defaultValue any
+
+---@class LassClassDefinition
+---@field variables table<string, LassVariableDefinition>
+
+-- The stuff that makes Lass churn -----------------------------------------------------------------
+
+---@type table<string, LassClassDefinition>
 lass.definedClasses = {}
+
+---@param parents string[]
+local function checkParentValidity(parents)
+    for i = 1, #parents do
+        local parentName = parents[i]
+        if not lass.definedClasses[parentName] then
+            error("Trying to inherit from a class that hasn't been defined ('" .. parentName .. "')", 4)
+        end
+    end
+end
+
+---@param varName string
+---@return string varName
+---@return table prefixesFound
+local function extractPrefixesFromVariable(varName)
+    local prefixesFound = {}
+    local prefixes, remainder = string.match(varName, "^(.-)___*(.*)")
+    if prefixes then
+        prefixes = prefixes .. "_"
+        varName = remainder
+        for prefix in string.gmatch(prefixes, "(.-)_") do
+            prefixesFound[prefix] = true
+        end
+    end
+    return varName, prefixesFound
+end
+
+local prefixValid = {
+    private = true,
+    public = true
+}
+
+local function registerClassVariablesFromBody(className, classBody)
+    local classDefinition = lass.definedClasses[className]
+
+    local varNamesUsed = {}
+    for varName, varValue in pairs(classBody) do
+        local prefixes
+        varName, prefixes = extractPrefixesFromVariable(varName)
+        local noModifier = not next(prefixes)
+        local accessLevel = prefixes["private"] and "private" or "public"
+
+        -- Check for nonsense
+        if prefixes.private and prefixes.public then
+            error("Variable '" .. varName .. "' is attempting to be public and private at the same time", 4)
+        end
+        for prefix in pairs(prefixes) do
+            if not prefixValid[prefix] then
+                error("Unknown access modifier '" .. prefix .. "' in variable '" .. varName .. "'", 4)
+            end
+        end
+
+        -- Make sure there's no duplicate variable names
+        if varNamesUsed[varName] then
+            error("Duplicate variable '" .. varName .. "'", 4)
+        end
+        varNamesUsed[varName] = true
+
+        -- Make sure a variable's access level can't be changed
+        if not noModifier and classDefinition.variables[varName] then
+            if classDefinition.variables[varName].accessLevel ~= accessLevel then
+                error("Attempting to change access level of variable '" .. varName .. "' from " .. classDefinition.variables[varName].accessLevel .. " to " .. accessLevel, 4)
+            end
+        end
+
+        -- Add to class
+        if classDefinition.variables[varName] then
+            classDefinition.variables[varName].defaultValue = varValue
+        else
+            classDefinition.variables[varName] = {accessLevel = accessLevel, defaultValue = varValue}
+        end
+    end
+end
+
+---@param className string
+---@param parents string[]
+---@param classBody table
+local function defineClass(className, parents, classBody)
+    checkParentValidity(parents)
+
+    ---@type LassClassDefinition
+    local classDefinition = {
+        variables = {},
+    }
+    lass.definedClasses[className] = classDefinition
+
+    -- Inherit variables
+    for parentIndex = 1, #parents do
+        local parentName = parents[parentIndex]
+        local parentClassDefinition = lass.definedClasses[parentName]
+        for varName, varValue in pairs(parentClassDefinition.variables) do
+            classDefinition.variables[varName] = {accessLevel = varValue.accessLevel, defaultValue = varValue.defaultValue}
+        end
+    end
+
+    registerClassVariablesFromBody(className, classBody)
+end
 
 -- The meat of the syntax --------------------------------------------------------------------------
 
@@ -86,9 +193,9 @@ function classMakingMetatable:__call(classBody)
     local className = classMakingTable.upcomingClassName
     assertWithLevel(className, "A class is being defined but no name for it was found. Are you using the library in weird ways? Unexpected behaviour might arise if you split class creation into multiple lines. Please use:\nlass 'Class' { }")
 
-    print(className, ":", unpack(parents))
-    lass.definedClasses[className] = true
     classMakingTable.upcomingClassName = nil
+
+    defineClass(className, parents, classBody)
 end
 
 -- Lass metatable metamethods and such go here -----------------------------------------------------
